f1de79ca18e922e590234d86ec6f4d66b5a0c0e5
Junya Takaesu <takaesu_junya@ca-adv.co.jp>
2024-08-28 21:12:07 +0900

    ログイン機能実装してみた by takaesu

---
+++ app/Logout.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "../firebase";
+
+interface HomePageProps {
+  email?: string;
+}
+
+export default function Logout({ email }: HomePageProps) {
+  const router = useRouter();
+
+  async function handleLogout() {
+    await signOut(getAuth(app));
+
+    await fetch("/api/logout");
+
+    router.push("/login");
+  }
+
+  return (
+    <section className="mt-10">
+      <button
+        onClick={handleLogout}
+        className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
+      >
+        Logout
+      </button>
+    </section>
+  );
+}

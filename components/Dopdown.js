a61fb3072df8468c088d98182fe5c542e177077a
e23007 <e23007@std.it-college.ac.jp>
2024-08-30 10:17:13 +0900

    doublequoteに変更

--- components/Dopdown.js
+++ components/Dopdown.js
@@ -1,11 +1,11 @@
 // app/components/Dropdown.js
-import { Select } from '@shadcn/ui';
+import { Select } from "@shadcn/ui";
 
 export default function Dropdown() {
     const options = [
-        { label: 'Option 1', value: 'option1' },
-        { label: 'Option 2', value: 'option2' },
-        { label: 'Option 3', value: 'option3' },
+        { label: "Option 1", value: "option1" },
+        { label: "Option 2", value: "option2" },
+        { label: "Option 3", value: "option3" },
     ];
 
     return (
--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export const Select = ({ children }) => {
     return <div className="select">{children}</div>;
